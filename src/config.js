6cb6809db3bb20f88e68f03a498009dacad6cc69
Miguel agudelo <agudelomiguel803-pixel>
2026-03-16 15:48:44 -0500

    Clase_11B_Agenda_ADSO_v10_Version_PRO_UI

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL = "http://localhost:3002/contactos"; 
 
 export const APP_INFO = {
-  titulo: "Agenda ADSO V9",
+  titulo: "Agenda ADSO V10",
   subtitulo: "Gestión de contactos",
   ficha: "3229209", 
 };
